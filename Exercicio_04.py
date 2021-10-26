c44f5118779901af9e885cae203c74dfa6632a94
AdelinoSantos <adv.adelinos@gmail.com>
2021-10-26 11:37:25 -0300

    Create Exercicio_04.py

---
+++ Exercicio_04.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite a nota do primeiro bimestre: \n"))
+num2 = int(input("Digite a nota do segundo bimestre: \n"))
+num3 = int(input("Digite a nota do terceiro bimestre: \n"))
+num4 = int(input("Digite a nota do quarto bimestre: \n"))
+media = (num1+num2+num3+num4)/4
+print("A media das notas é",media)
